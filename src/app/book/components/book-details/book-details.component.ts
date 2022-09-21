10ae66b5af1514ac9e4ac328d55b334c74ea511c
Marek Matczak <marek.matczak79@gmail.com>
2022-09-21 12:24:54 +0200

    property and event bindings

--- src/app/book/components/book-details/book-details.component.ts
+++ src/app/book/components/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Book} from '../../model/book';
 
 @Component({
   selector: 'ba-book-details',
@@ -6,4 +7,20 @@ import {Component} from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
+  readonly book: Book = {
+    author: 'Douglas Crockford',
+    title: 'JavaScript.The Good Parts'
+  };
+
+  save(event: Event) {
+    event.preventDefault();
+    const bookForm = event.target as HTMLFormElement;
+    const authorInput = bookForm.querySelector<HTMLInputElement>('#author');
+    const titleInput = bookForm.querySelector<HTMLInputElement>('#title');
+    const updatedBook: Book = {
+      author: authorInput?.value ?? '',
+      title: titleInput?.value ?? ''
+    };
+    console.log(updatedBook);
+  }
 }
