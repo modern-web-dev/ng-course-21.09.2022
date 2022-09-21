931ef4b60383a2cb7fb2611548441ec484d52796
Marek Matczak <marek.matczak79@gmail.com>
2022-09-21 11:29:07 +0200

    BookDetails initially added

---
+++ src/app/book/components/book-details/book-details.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ba-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent {
+}
