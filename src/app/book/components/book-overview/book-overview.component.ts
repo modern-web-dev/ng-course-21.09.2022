49ee1ec25952ec6050bb8d88950a5938c5f51a64
Tomasz Wawrzyniak <tomasz.wawrzyniak@capgemini.com>
2022-09-22 11:55:06 +0200

    05-service-part-2

--- src/app/book/components/book-overview/book-overview.component.ts
+++ src/app/book/components/book-overview/book-overview.component.ts
@@ -1,23 +1,63 @@
-import {Component, OnDestroy} from '@angular/core';
-import {Book} from '../../model/book';
-import {BookService} from '../../services/book.service';
-import {Observable, Subscription} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { Book } from '../../model/book';
+import { BookService } from '../../services/book.service';
+import {
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  Observable,
+  OperatorFunction,
+  switchMap,
+  Subject,
+  takeUntil,
+  merge,
+} from 'rxjs';
 
 @Component({
   selector: 'ba-book-overview',
   templateUrl: './book-overview.component.html',
-  styleUrls: ['./book-overview.component.scss']
+  styleUrls: ['./book-overview.component.scss'],
+  providers:[
+    BookService
+  ],
 })
-export class BookOverviewComponent implements OnDestroy {
+export class BookOverviewComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('typeahead')
+  typeahead?: ElementRef<HTMLInputElement>;
+
   selectedBook: Book | null = null;
+  books$: Observable<Book[]> | undefined;
+  booksLength$: Observable<number> | undefined;
+  private readonly unsubscribe$: Subject<true> = new Subject<true>();
 
-  readonly books$: Observable<Book[]>;
-  private readonly bookService: BookService;
-  private readonly subscriptions: Subscription[] = [];
+  constructor(private readonly bookService: BookService) {
+  }
+  typeaheadElems$?: Observable<Book[]>;
+  ngAfterViewInit() {
+    if (!this.typeahead) {
+      throw new Error('Could not find typeahead');
+    }
 
-  constructor() {
-    this.bookService = new BookService();
-    this.books$ = this.bookService.getAll();
+    const typeaheadBooks = fromEvent(
+      this.typeahead.nativeElement,
+      'input'
+    ).pipe(
+      mapEventToInputValue(),
+      distinctUntilChanged(),
+      switchMap((query) => this.bookService.findBooks(query)),
+      takeUntil(this.unsubscribe$)
+    );
+
+    this.books$ = merge(this.bookService.getAll(), typeaheadBooks);
+    setTimeout(()=> {
+      this.booksLength$ = this.books$?.pipe(map((books) => books.length));
+    },0);
   }
 
   selectBookOf(book: Book) {
@@ -29,12 +69,21 @@ export class BookOverviewComponent implements OnDestroy {
   }
 
   updateBook(updatedBook: Book) {
-    this.subscriptions.push(
-      this.bookService.updateBook(updatedBook).subscribe(updatedBook => this.selectedBook = updatedBook)
-    );
+    this.bookService
+      .updateBook(updatedBook)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((updatedBook) => (this.selectedBook = updatedBook));
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
   }
+}
+
+function mapEventToInputValue(): OperatorFunction<Event, string> {
+  return map((event: Event) => {
+    const input = event.target as HTMLInputElement;
+    return input.value;
+  });
 }
--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,38 +1,55 @@
-import {Book} from '../model/book';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { Book } from '../model/book';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class BookService {
   private readonly booksSubject = new BehaviorSubject<Book[]>([
     {
       id: 0,
       author: 'Douglas Crockford',
-      title: 'JavaScript.The Good Parts'
+      title: 'JavaScript.The Good Parts',
     },
     {
       id: 1,
       author: 'Gang of Four',
-      title: 'Design Patterns'
+      title: 'Design Patterns',
     },
     {
       id: 2,
       author: 'Robert C. Martin',
-      title: 'Clean Code'
-    }
+      title: 'Clean Code',
+    },
   ]);
   private readonly books$ = this.booksSubject.asObservable();
 
   getAll(): Observable<Book[]> {
+    console.log('getAll')
     return this.books$;
   }
 
   updateBook(bookToUpdate: Book): Observable<Book> {
-    return new Observable<Book>(subscriber => {
-      const updatedBookCopy = {...bookToUpdate};
+    return new Observable<Book>((subscriber) => {
+      const updatedBookCopy = { ...bookToUpdate };
       const currentBooks = this.booksSubject.getValue();
-      const newBooks = currentBooks.map(
-        book => book.id === bookToUpdate.id ? updatedBookCopy : book);
+      const newBooks = currentBooks.map((book) =>
+        book.id === bookToUpdate.id ? updatedBookCopy : book
+      );
       this.booksSubject.next(newBooks);
       subscriber.next(updatedBookCopy);
+      subscriber.complete();
+    });
+  }
+
+  findBooks(query: string): Observable<Book[]> {
+    return new Observable<Book[]>((subscriber) => {
+      
+    console.log('findBooks')
+      const books = this.booksSubject.getValue();
+      const filteredBooks = books.filter(
+        (book) => book.title.indexOf(query) != -1
+      );
+      subscriber.next(filteredBooks);
       subscriber.complete();
     });
   }
